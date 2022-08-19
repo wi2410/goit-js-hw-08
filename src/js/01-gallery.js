dc91d61bce3b79091750a59c276a96b27a366a67
wi2410 <101503219+wi2410@users.noreply.github.com>
2022-08-19 20:12:05 +0300

    Task 1 solve

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,3 +5,25 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+const galleryItemsContainer = document.querySelector('.gallery');
+const cardsMarkup = createGaleryCardsMarkup(galleryItems);
+
+galleryItemsContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+function createGaleryCardsMarkup(gallery) {
+    return gallery.map(({ preview, original, description }) => {
+        return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    }).join('');  
+    
+};
+
+
+let gallery = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
+gallery.on('show.simplelightbox');
