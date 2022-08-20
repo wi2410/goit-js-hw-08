541f62cd5b9b01436b51e3b4203fa43cb2dc2563
wi2410 <101503219+wi2410@users.noreply.github.com>
2022-08-20 19:36:37 +0300

    Task 2 solved

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+    
+const onPlay = function({seconds}) {
+    // data is an object containing properties specific to that event
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(seconds));
+};
+
+const getTimeFromStorage = localStorage.getItem(STORAGE_KEY);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+player.setCurrentTime(JSON.parse(getTimeFromStorage));
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,4 +1,4 @@
-import throttle from 'lodash.throttle'
+import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = 'feedback-form-state';
 
