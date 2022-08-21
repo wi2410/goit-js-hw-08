af25b18d1c6d894e748c1d4385a9b066dd0a7e03
wi2410 <101503219+wi2410@users.noreply.github.com>
2022-08-21 23:57:55 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,29 +2,31 @@ import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = 'feedback-form-state';
 
-const formData = {
-    email: '',
-    message: '',
-};
+
 
 const refs = {
     form: document.querySelector('.feedback-form'),
     textarea: document.querySelector('.feedback-form textarea'),
     input: document.querySelector('.feedback-form input'),
 }
+const savedFormData = localStorage.getItem(STORAGE_KEY);
+const elem = refs.form.elements;
+const parsedData = JSON.parse(savedFormData);
+
+const formData = parsedData || {
+    email:'',
+    message: '',
+};
 
 refs.form.addEventListener('submit', onFormSubmit)
 refs.form.addEventListener('input', throttle(onFormInput, 500))
 
-const savedFormData = localStorage.getItem(STORAGE_KEY);
-const elem = refs.form.elements;
-const parsedData = JSON.parse(savedFormData);
+
 
 populateFeedbackForm();
 
 function onFormInput(ev) {
     formData[ev.target.name] = ev.target.value;
-    console.log(formData);
     localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
@@ -48,7 +50,7 @@ function onFormSubmit(ev) {
 
 function populateFeedbackForm() {
     if (savedFormData) {
-        
+
         Object.keys(parsedData).forEach((key) => {elem[key].value = parsedData[key]})
     }
 }
